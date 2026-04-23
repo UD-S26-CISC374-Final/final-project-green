abc5898ff653884dc89c4adc3da2d6b67fee593e
Andrew Shaffer <dshaffer05@icloud.com>
2026-04-23 03:56:34 -0400

    added the note with functionality

--- src/game/scenes/game-over.ts
+++ src/game/scenes/game-over.ts
@@ -32,7 +32,7 @@ export class GameOver extends Scene {
             .setDepth(100);
 
         this.scoreText = this.add
-            .text(512, 450, `Final Score: ${SCORE}/4`, {
+            .text(512, 450, `Final Score: ${SCORE}/5`, {
                 fontFamily: "Arial Black",
                 fontSize: 32,
                 color: "#ffffff",
